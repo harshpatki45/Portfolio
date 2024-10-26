8c06f515ce90675543f63de3ac01b78d9197e9d5
harshpatki45 <patkiharsh3@gmail.com>
2024-10-26 17:01:25 +0530

    first

---
+++ src/constants/index.js
@@ -0,0 +1,55 @@
+import project1 from "../assets/projects/project1.svg";
+import project2 from "../assets/projects/project2.jpg";
+import project3 from "../assets/projects/project3.jpg";
+import project4 from "../assets/projects/project4.jpg";
+
+export const HERO_CONTENT = ` A passionate Full Stack Developer with expertise in building dynamic, bring creativity, attention to detail, and a strong enthusiasm for crafting visually stunning and user-friendly web applications. I love transforming ideas into interactive, responsive designs. My goal is to build seamless digital experiences that not only look great but also provide intuitive functionality. Whether it's a sleek portfolio or a dynamic web app, I am eager to collaborate and grow in this ever-evolving field of full stack development. Let's create something amazing together!`;
+
+export const ABOUT_TEXT =  `I am a dedicated full stack developer currently in my 4th year of studying Computer Science.I have gained hands-on experience through projects such as Amazon Clone and Employee Management System, utilizing React, Django, and various databases. Currently, I am developing the IMC portal, which is helping me refine my project management and collaboration skills. I thrive in team environments and enjoy tackling complex challenges, always striving for innovative solutions. Outside of technology, I'm passionate about cricket and love engaging in outdoor activities, which keep my energy high and my competitive spirit alive! I also have an enthusiastic interest in entrepreneurship and emerging tech trends, always eager to explore innovative ideas and opportunities that shape the future. I look forward to connecting with like-minded professionals and contributing to impactful projects.`;
+
+
+
+export const PROJECTS = [
+  {
+    title: "Cipher Vault",
+    image: project1,
+    description:
+      "Cipher Vault is a web app for encrypting and decrypting text using ciphers like Caesar, Vigenère, and Atbash.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Fitness Exercise App",
+    image: project2,
+    description:
+      " Built a website which gives us details of all the exercises with the bodypart it is focusing on.The website also gives us the videos related to the exercises.",
+    technologies: ["React", "Material-UI", "Rapid-Api"],
+  },
+  {
+    title: "Office Employee Management System",
+    image: project3,
+    description:
+      "Built a system to manage all the emoloyees working at a organization. This system can be used by HR like people.",
+    technologies: ["HTML", "CSS", "Django", "Bootstrap"],
+  },
+  {
+    title: "Amazon Website Clone",
+    image: project4,
+    description:
+      "A web application that replicates Amazon shopping website.",
+    technologies: ["HTML", "CSS", "javascript"],
+  },
+];
+
+export const EXPERIENCE = [{
+  year: "August 2024 - September 2024",
+  role: "Frontent Developer Intern",
+  company: "The Entrepreneurship Network",
+  description: "I worked as a Frontend Developer during a remote internship, utilizing React to build dynamic user interfaces. This experience helped me deepen my knowledge of modern web development, including component-based architecture and state management. I also honed my collaboration and problem-solving skills while working in a distributed team environment. ",
+  technologies: ["React", "HTML", "CSS", "Javascript"]
+}];
+
+export const CONTACT = {
+  address: "Address: 12, Ganesh Colony, Rambagh(Near Dadawadi), Indore, Madhya Pradesh 452007, India",
+  phoneNo: "Phone: +91 8103183490",
+  email: "patkiharsh2@gmail.com",
+};
